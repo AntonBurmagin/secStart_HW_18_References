db172c551f7077eab29cab490eb5a875060ed3f2
Anton Burmagin <burmagin.anton.s@gmail.com>
2024-04-22 21:45:45 +0300

    second commit: add solution to 2nd task - Ladder and rabbit

--- main.cpp
+++ main.cpp
@@ -10,6 +10,22 @@ void swapVec (std::vector<int> &v, int b[]) {
     }
 }
 
+int upstairWays (int steps, int jump) {
+    if (steps <= jump) {
+        int sum = 1;
+        for (int j = steps; j > 1; j--) {
+              sum *= 2;
+        }
+        return sum;
+    } else {
+        int sum = 0;
+        for (int j = jump; j > 0; j--) {
+            sum += upstairWays(steps - j, jump);
+        }
+        return sum;
+    }
+}
+
 int main() {
     //18.5.1 swap vector and array
     /*
@@ -26,6 +42,19 @@ int main() {
     for(int i = 0; i < 4; ++i)
         std::cout << b[i];
     */
+    // 18.5.2 rabbit and ladder: how many ways to the top
+    while (true) {
+        int steps, jumpMax;
+        std::cout << "Input ladder steps and longest possible jump:" << std::endl;
+        std::cin >> steps >> jumpMax;
+        if (steps > 0 && jumpMax > 0) {
+            std::cout << "There are " << upstairWays(steps, jumpMax) << " ways to top!" << std::endl;
+        } else {
+            std::cout << "Incorrect input! Try again!" << std::endl;
+        }
+    }
+
+    // 18.5.3 
 
     return 0;
 }
